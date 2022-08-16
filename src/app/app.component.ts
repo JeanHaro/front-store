758d16b0679818fb6e2358f11d4b7d9a4d8ce4ca
Jean Haro <JeanCHL98@hotmail.com>
2022-08-16 14:34:56 -0500

    ngFor y ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// Interface
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'store';
+
+  // FIXME: USO DE ngFor PARA RECORRER OBJETOS
+  // Productos
+  products: Product[] = [{
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '4',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+  ]
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,10 @@
+// FIXME: USO DE ngFor PARA RECORRER OBJETOS
+// TODO: Es una representación de lo que tendrá el producto
+// TODO: export - es para exportar la interface
+export interface Product {
+    id: string;
+    image: string;
+    title: string;
+    price: number;
+    description: string;
+}
