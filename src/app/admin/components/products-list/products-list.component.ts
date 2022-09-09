d9e5b0ab26b98e98fb74c85a2f075e972da30909
Jean Haro <JeanCHL98@hotmail.com>
2022-09-09 04:11:57 -0500

    Cierre de ejercicio de detalle

--- src/app/admin/components/products-list/products-list.component.ts
+++ src/app/admin/components/products-list/products-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductsService } from 'src/app/core/services/products/products.service';
 
+// FIXME: CIERRE DE EJERCICIO DE DETALLE
+import { Product } from 'src/app/core/models/product.model';
+
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
@@ -9,21 +12,39 @@ import { ProductsService } from 'src/app/core/services/products/products.service
 })
 export class ProductsListComponent implements OnInit {
 
-  products = [];
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+  ];
+  // TODO: Esto se saca de Angular Material
   displayedColumns: string[] = ['id', 'title', 'price', 'actions'];
 
 
   constructor (private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.fetchProducts();
   }
 
   // FIXME: LISTA DE INVENTARIO Y DETALLE
   // TODO: Para mandar datos
-  fetchProducts() {
+  fetchProducts(): void {
     this.productsService.getAllProducts()
-    .subscribe(products => {
-      this.products = this.products;
+    .subscribe((products) => {
+      this.products = products;
+    })
+  }
+
+  // TODO: Eliminar producto
+  deleteProduct (id: string) {
+    this.productsService.deleteProduct(id)
+    .subscribe(rta => {
+      this.fetchProducts();
     })
   }
 }
