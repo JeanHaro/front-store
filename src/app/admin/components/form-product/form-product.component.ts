23f7ecde08a08ccfeaf13137a6ef50a75c07059c
Jean Haro <JeanCHL98@hotmail.com>
2022-09-11 03:32:10 -0500

    Ajustar estilos en un formulario

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 // FIXME: CREANDO EL FORMULARIO DE PRODUCTOS
 // TODO: FormGroup - es un grupo de controles
 // TODO: FormBuilder - es una extensión de Angular, que nos sirve para crear ese grupo rápidamente
-import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+// Servicios
+import { ProductsService } from 'src/app/core/services/products/products.service';
 
 @Component({
   selector: 'app-form-product',
@@ -14,9 +18,32 @@ export class FormProductComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor (private formBuilder: FormBuilder) { }
+  constructor (
+    private formBuilder: FormBuilder,
+    private productService: ProductsService,
+    private router: Router
+  ) { 
+    this.buildForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  // FIXME: AJUSTAR ESTILOS EN UN FORMULARIO
+  saveProduct (event: Event) {
+    // preventDefault() - quite la acción por defecto, para que no recargue la página
+    event.preventDefault();
+    // console.log(this.form.value);
+    if (this.form.valid) {
+      const product = this.form.value;
+
+      this.productService.createProduct(product)
+      .subscribe((newProduct) => {
+        console.log(newProduct);
+        // TODO: Una ruta que le damos
+        this.router.navigate(['./admin/products'])
+      });
+    }
   }
 
   private buildForm() {
